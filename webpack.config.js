bbda41bcac8521d54b46c47e333ad0512177d5f8
Ruben Cardenas <rubencg88@gmail.com>
2025-04-06 18:32:38 -0400

    Added firebase deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -28,6 +29,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'public/images', to: 'images' },
+        { from: 'public/videos', to: 'videos' },
+      ],
     }),
   ],
   devServer: {
